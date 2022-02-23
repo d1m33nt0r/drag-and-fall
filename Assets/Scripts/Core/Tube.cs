0344bea59e900f5893e619c0513f994533a2e524
gribdmitriy <dmitrii.9rib@gmail.com>
2022-02-23 14:28:21 +0200

    Initilization platforms and position points of platforms

--- Assets/Scripts/Core/Tube.cs
+++ Assets/Scripts/Core/Tube.cs
@@ -1,16 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using Core;
 using UnityEngine;
 
 public class Tube : MonoBehaviour
 {
+    [SerializeField] private GameObject platformPrefab;
+    [SerializeField] private int countPlatforms;
+    [SerializeField] private float distanceBetweenPlatforms;
     
-    void Start()
+    private Platform[] platforms;
+    private Vector3[] localPositionsOfPlatforms;
+    
+    private void Start()
     {
-        
+        InitializePlatformPoints();
+        InitializePlatforms();
     }
 
-    void Update()
+    private void InitializePlatformPoints()
+    {
+        localPositionsOfPlatforms = new Vector3[countPlatforms];
+        
+        for (var i = 0; i < countPlatforms; i++)
+        {
+            localPositionsOfPlatforms[i] = new Vector3(transform.position.x, transform.position.y - distanceBetweenPlatforms * i, transform.position.z);
+        }
+    }
+    
+    private void InitializePlatforms()
+    {
+        platforms = new Platform[countPlatforms];
+        
+        for (var i = 0; i < countPlatforms; i++)
+        {
+            var platformInstance = Instantiate(platformPrefab);
+            platformInstance.transform.position = transform.TransformPoint(localPositionsOfPlatforms[i]);
+            platforms[i] = platformInstance.GetComponent<Platform>();
+        }
+    }
+
+    
+
+    public void MovePlatforms()
     {
         
     }
