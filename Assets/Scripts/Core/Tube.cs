fdf50206ecca8a9b8f348c3788551eddcc5f0da1
gribdmitriy <dmitrii.9rib@gmail.com>
2022-02-23 14:51:58 +0200

    Basic implementation of platform movement mechanics

--- Assets/Scripts/Core/Tube.cs
+++ Assets/Scripts/Core/Tube.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using Core;
+using DG.Tweening;
 using UnityEngine;
 
 public class Tube : MonoBehaviour
@@ -6,6 +8,7 @@ public class Tube : MonoBehaviour
     [SerializeField] private GameObject platformPrefab;
     [SerializeField] private int countPlatforms;
     [SerializeField] private float distanceBetweenPlatforms;
+    [SerializeField] private float platformMovementSpeed;
     
     private Platform[] platforms;
     private Vector3[] localPositionsOfPlatforms;
@@ -14,8 +17,20 @@ public class Tube : MonoBehaviour
     {
         InitializePlatformPoints();
         InitializePlatforms();
+
+        StartCoroutine(SimulatePlatformsMoving());
     }
 
+    private IEnumerator SimulatePlatformsMoving()
+    {
+        while (true)
+        {
+            MovePlatforms();
+            
+            yield return new WaitForSeconds(1);
+        }
+    }
+    
     private void InitializePlatformPoints()
     {
         localPositionsOfPlatforms = new Vector3[countPlatforms];
@@ -31,19 +46,25 @@ public class Tube : MonoBehaviour
         platforms = new Platform[countPlatforms];
         
         for (var i = 0; i < countPlatforms; i++)
-        {
-            var platformInstance = Instantiate(platformPrefab, localPositionsOfPlatforms[i], Quaternion.identity, transform);
-            platforms[i] = platformInstance.GetComponent<Platform>();
-        }
+            CreateNewPlatform(i);
     }
     
     public void MovePlatforms()
     {
-        Destroy(platforms[0]);
+        Destroy(platforms[0].gameObject);
         
         for (var i = 1; i < countPlatforms; i++)
         {
-            
+            platforms[i - 1] = platforms[i];
+            platforms[i - 1].transform.DOLocalMoveY(localPositionsOfPlatforms[i - 1].y, platformMovementSpeed);
         }
+        
+        CreateNewPlatform(countPlatforms - 1);
+    }
+
+    private void CreateNewPlatform(int _platformIndex)
+    {
+        var platformInstance = Instantiate(platformPrefab, localPositionsOfPlatforms[_platformIndex], Quaternion.identity, transform);
+        platforms[_platformIndex] = platformInstance.GetComponent<Platform>();
     }
 }
